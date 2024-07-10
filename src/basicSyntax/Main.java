dcf25661139a3dfda991b2d2f9816e9cef7ccf92
TVKEO <t.nagatani.cingroup2012@gmail.com>
2024-07-10 10:21:45 +0900

    marge

--- src/basicSyntax/Main.java
+++ src/basicSyntax/Main.java
@@ -1,10 +1,8 @@
 package basicSyntax;
 
-// クラスブロック
 public class Main {
-  // メソッドブロック
-  public static void main(String[] args) {
-    // コードブロック(ここに処理を書いていきます。)
-    System.out.println("Hello Geekation");
-  }
+	public static void main(String[] args) {
+	    System.out.println("Hello Geekation");
+
+	}
 }
