954e84e6f21b719b50db52bbacf832011d4e85e7
TVKEO <t.nagatani.cingroup2012@gmail.com>
2024-07-10 12:18:57 +0900

    ifSwitch修正

--- src/basicSyntax/IfSwitch.java
+++ src/basicSyntax/IfSwitch.java
@@ -3,31 +3,31 @@ package basicSyntax;
 public class IfSwitch {
 
 	public static void main(String[] args) {
-		int num=8;
-		
-		if(num%2==0) {
+		int num = 8;
+
+		if (num % 2 == 0) {
 			System.out.println("2の倍数です。");
-		}else if(num%3==0) {
+		} else if (num % 3 == 0) {
 			System.out.println("3の倍数です。");
-		}else {
+		} else {
 			System.out.println("どちらでもありません。");
-			
+
 		}
-		
-		String pref="岡山";
-		
-		switch(pref) {
-			case "北海":
-				System.out.println("北海道");
+
+		String pref = "岡山";
+
+		switch (pref) {
+		case "北海":
+			System.out.println("北海道");
 			break;
-			case "東京":
-				System.out.println("東京都");
+		case "東京":
+			System.out.println("東京都");
 			break;
-			case "京都":	
-				System.out.println("京都府");
+		case "京都":
+			System.out.println("京都府");
 			break;
-			default:
-				System.out.println("当てはまりません。");
+		default:
+			System.out.println("当てはまりません。");
 
 		}
 
