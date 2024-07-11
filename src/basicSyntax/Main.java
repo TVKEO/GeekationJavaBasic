d7bbc18ebea7ba3ba427d46e4c45dc09b77cecd7
TVKEO <t.nagatani.cingroup2012@gmail.com>
2024-07-11 17:24:26 +0900

    GeekIntroduction1

--- src/basicSyntax/Main.java
+++ src/basicSyntax/Main.java
@@ -3,6 +3,19 @@ package basicSyntax;
 public class Main {
 	public static void main(String[] args) {
 	    System.out.println("Hello Geekation");
-
+	    
+	    
+	    int age = 15;
+	    double height=120.5;
+	    int num1 =24;
+	    int num2=100;
+	  
+	    GeekIntroduction.greeting (age,height);
+	    GeekIntroduction.specialSkill(num1,num2);
 	}
 }
+
+
+
+//引数1: int age (年齢)
+//引数2: double height (身長)
