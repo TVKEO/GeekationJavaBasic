c179c19b7303e9a42da0399e9c993b300849a8c4
TVKEO <t.nagatani.cingroup2012@gmail.com>
2024-07-16 11:13:12 +0900

    ObjectExcercise修正

--- src/objectExercise/Land.java
+++ src/objectExercise/Land.java
@@ -1,23 +1,25 @@
 package objectExercise;
 
-	public class Land extends Property{
-		private double widths;
-	
-		Land(String name, String user, String category, int price,double widths){
-			super(name, user,category,price);
-			this.widths=widths;		
-	}
-		public void info() {
-			super.info();
-			System.out.println("広さ："+this.widths+"㎡");
+public class Land extends Property {
+	private double widths;
 
-		
-		}
-		public double getWidths (){
-			return this.widths;
-		}
-		
-		public void setWidths(double widths) {
-			this.widths = widths;
-		
-		}}
+	Land(String name, String user, String category, int price, double widths) {
+		super(name, user, category, price);
+		this.widths = widths;
+	}
+
+	public void info() {
+		super.info();
+		System.out.println("広さ：" + this.widths + "㎡");
+
+	}
+
+	public double getWidths() {
+		return this.widths;
+	}
+
+	public void setWidths(double widths) {
+		this.widths = widths;
+
+	}
+}
